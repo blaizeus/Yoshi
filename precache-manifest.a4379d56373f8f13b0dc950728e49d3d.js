7b6dbaa410ba1859a395b897f742c8bf73792041
Blaise Vellozzo <bvellozzo@gmail.com>
2020-10-23 21:36:50 +0200

    Updates

---
+++ precache-manifest.a4379d56373f8f13b0dc950728e49d3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc14e20b6d283c9fd9006ae37077b3ea",
+    "url": "/Yoshi/index.html"
+  },
+  {
+    "revision": "73e800ab953ae426fa46",
+    "url": "/Yoshi/static/js/2.48bde05a.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Yoshi/static/js/2.48bde05a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9626cd8d42271e2daffd",
+    "url": "/Yoshi/static/js/main.6ac477a0.chunk.js"
+  },
+  {
+    "revision": "01400ef2591a63896bf1",
+    "url": "/Yoshi/static/js/runtime-main.737dfa59.js"
+  }
+]);
